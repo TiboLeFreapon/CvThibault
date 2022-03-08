b84eaf24c849c27b167308424523be2f5120edd3
thibault SCHMITT <schmitt.thibault34070@gmail.com>
2022-03-08 19:55:46 +0100

    Nouveau deploiment preprod

--- deploimentpreprod.sh
+++ deploimentpreprod.sh
@@ -2,7 +2,7 @@ git add *
 
 git commit -m "Nouveau deploiment preprod"
 
-ssh -p 65002 u558905572@145.14.151.201 'rm public_html/css/* public_html/stye.css public_html/js/* public_html/img/* public_html/index.html'
+ssh -p 65002 u558905572@145.14.151.201 'rm public_html/cv/css/* public_html/cv/stye.css public_html/cv/js/* public_html/cv/img/* public_html/cv/index.html'
 
 yarn build
 
